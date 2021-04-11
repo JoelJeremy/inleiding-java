a5414937befd75719757c3fff3b5f9fce3004a66
JoelJeremy <amahj@roc-dev.com>
2021-04-11 15:23:16 +0200

    chasnge text from "hallo klas 8A" to "hallo cohort jj0"

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo klas cohort j0!");
+        System.out.println("Hallo klas cohort ja0!");
     }
 }
